550391cced7c96ae6d2747b3401e1560d2dcc6b1
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2021-09-10 23:54:48 +0200

    'reviews' views and templates updated for subscriptions

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import CreateView
 from accounts.forms import SignupForm, LoginForm
 
@@ -8,12 +9,23 @@ from accounts.forms import SignupForm, LoginForm
 # Create your views here.
 class SignupView(CreateView):
     form_class = SignupForm
-    context_object_name = "user_form"
     template_name = "accounts/signup.html"
     success_url = "../account-created/"
 
+    def form_valid(self, form):
+        # Override form_valid function from parent class to add automatic login
+        # after signup
+        form.save()
+        username = self.request.POST["username"]
+        password = self.request.POST["password1"]
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+        return HttpResponseRedirect(reverse('accounts:index'))
+
 
 def index_view(request):
+    form = LoginForm
+    context = {"form": form}
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -21,14 +33,17 @@ def index_view(request):
         if user is not None:
             login(request, user)
             return redirect("accounts:index")
-    else:
-        form = LoginForm
-    context = {"form": form}
     return render(request, "accounts/index.html", context)
 
 
 def account_created(request):
     context = {}
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
     return render(request, "accounts/account-created.html", context)
 
 
--- src/reviews/views.py
+++ src/reviews/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+from accounts.models import CustomUser
+from reviews.models import UserFollows
+
 
 # Create your views here.
 def subscriptions(request):
-    context = {}
+    current_user = request.user.username
+    current_user_id = CustomUser.objects.values_list("id").get(username=current_user)
+    followers = UserFollows.objects.filter(followed_user_id=current_user_id).values()
+    followers = [follower["user_id"] for follower in followers]
+    followed_users = UserFollows.objects.filter(user_id=current_user_id).values()
+    followed_users = [user["followed_user_id"] for user in followed_users]
+    followers = [CustomUser.objects.get(pk=follower_id) for follower_id in followers]
+    followed_users = [CustomUser.objects.get(pk=user_id) for user_id in followed_users]
+    context = {"followers": followers,
+               "followed_users": followed_users}
     return render(request, "reviews/subscriptions.html", context)
