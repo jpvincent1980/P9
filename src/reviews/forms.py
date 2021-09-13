04f17c8f13c575e341ccf9a35671a074b10f0cdf
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2021-09-14 00:09:08 +0200

    Update ticket updated.

--- src/reviews/forms.py
+++ src/reviews/forms.py
@@ -1,5 +1,9 @@
 from django.forms import ModelForm, ChoiceField, RadioSelect
-from django.utils.translation import gettext_lazy as _
+from django.forms.widgets import ClearableFileInput, FileInput
+from django.utils.encoding import force_text
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _, ugettext_lazy
 
 from reviews.models import Review, Ticket
 
@@ -26,8 +30,33 @@ class TicketForm(ModelForm):
         labels = {"title": "Titre"}
 
 
+class CustomImageFieldWidget(FileInput):
+    initial_text = ugettext_lazy('Image actuelle')
+    input_text = ugettext_lazy('Nouvelle image')
+    template_with_initial = '<p>%(initial_text)s: </p><p>%(initial)s </p>' \
+                            '<p>%(input_text)s: </p><p>%(input)s </p>'
+    url_markup_template = '<img src="{0}" alt="{1}" width="{2}"/>'
+
+    def render(self, name, value, attrs=None, renderer=None):
+        substitutions = {'initial_text': self.initial_text,
+                         'input_text': self.input_text,}
+        template = '%(input)s'
+        substitutions['input'] = super(CustomImageFieldWidget, self).render(
+            name, value, attrs)
+
+        if value and hasattr(value, "url"):
+            template = self.template_with_initial
+            substitutions['initial'] = format_html(self.url_markup_template,
+                                                   value.url,
+                                                   force_text(value),
+                                                   400)
+
+        return mark_safe(template % substitutions)
+
+
 class UpdateTicketForm(ModelForm):
     class Meta:
         model = Ticket
         exclude = ["user"]
         labels = {"title": "Titre"}
+        widgets = {"image": CustomImageFieldWidget}
