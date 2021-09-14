ac926b75071bcaf0468ac0365ff65f31f187f5cf
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2021-09-14 12:09:01 +0200

    HTML snippets added + QuerySet annotations added + CSS updates

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -1,6 +1,7 @@
 from itertools import chain
 from operator import attrgetter
 
+from django.db.models import Value, CharField
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
@@ -15,8 +16,10 @@ class ReviewDetailView(DetailView):
 
 
 def create_review_view(request, ticket_id=None):
-    form = ReviewForm()
-    context = {"form": form,
+    review_form = ReviewForm()
+    ticket_form = TicketForm()
+    context = {"review_form": review_form,
+               "ticket_form": ticket_form,
                "ticket_id": ticket_id}
     return render(request, "reviews/review.html", context)
 
@@ -39,6 +42,11 @@ class CreateTicketView(CreateView):
     form_class = TicketForm
     template_name = "reviews/ticket.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(CreateTicketView, self).get_context_data(**kwargs)
+        context["ticket_form"] = context["form"]
+        return context
+
     def form_valid(self, form):
         ticket = form.save(commit=False)
         ticket.user = CustomUser.objects.get(pk=self.request.user.id)
@@ -50,6 +58,11 @@ class UpdateTicketView(UpdateView):
     model = Ticket
     form_class = UpdateTicketForm
     template_name = "reviews/ticket.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateTicketView, self).get_context_data(**kwargs)
+        context["ticket_form"] = context["form"]
+        return context
 
     def form_valid(self, form):
         ticket = form.save(commit=False)
@@ -66,7 +79,9 @@ def delete_ticket_view(request, pk):
 
 def posts_view(request):
     tickets = Ticket.objects.filter(user=request.user.id)
+    tickets.annotate(content_type=Value("Ticket", CharField()))
     reviews = Review.objects.filter(user=request.user.id)
+    reviews.annotate(content_type=Value("Review", CharField()))
     posts = sorted(chain(tickets, reviews), key=attrgetter("time_created"), reverse=True)
     context = {"posts": posts}
     return render(request, "reviews/posts.html", context)
