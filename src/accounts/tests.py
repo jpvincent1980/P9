f79b9f51e3ad2e8846a51f6931a8d99c4cac2a6b
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-03-16 15:42:06 +0100

    tests.py updated

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,13 +1,1 @@
-from django.test import TestCase
 
-
-# Create your tests here.
-class AccountsTest(TestCase):
-    """
-    A class that gathers tests for the index view.
-    """
-    def test_status_code(self):
-        """
-        Tests that the view returns a 200 status code.
-        """
-        self.assertEqual(200, 200)
