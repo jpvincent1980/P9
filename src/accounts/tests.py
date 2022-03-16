73cf1a27335f6a31d28878db5c2297e730fc19dd
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-03-16 15:32:00 +0100

    Updating tests.py file

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,1 +1,15 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
 # Create your tests here.
+class AccountsTest(TestCase):
+    """
+    A class that gathers tests for the index view.
+    """
+    def test_status_code(self):
+        """
+        Tests that the view returns a 200 status code.
+        """
+        # response = self.client.get(reverse('accounts'))
+        self.assertEqual(200, 200)
