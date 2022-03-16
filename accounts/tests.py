03ab0e61beaf013ef18215f3294bc3b3c59f90fd
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-03-16 17:55:46 +0100

    Added new tests to tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,2 +1,30 @@
-def test_status_code():
-    return True
+from django.test import TestCase
+from django.urls import reverse
+
+
+# Create your tests here.
+class IndexTest(TestCase):
+    """
+    A class that gathers tests for the index view.
+    """
+    def test_status_code(self):
+        """
+        Tests that the view returns a 200 status code.
+        """
+        response = self.client.get(reverse('accounts:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_used(self):
+        """
+        Tests that the view uses the correct template.
+        """
+        response = self.client.get(reverse('accounts:index'))
+        self.assertTemplateUsed(response, 'accounts/index.html')
+
+    def test_title(self):
+        """
+        Tests that 'LITReview' is included in the returned HTML
+        inside a <title></title> tag.
+        """
+        response = self.client.get(reverse('accounts:index'))
+        self.assertContains(response, "<title>LITReview</title>")
